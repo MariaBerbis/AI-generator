360012efa2dd47bbfed67d3fc78a11bc940bfae3
MariaBerbis <maria.berbis.m@gmail.com>
2023-12-27 19:20:45 +0000

    added more js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 function generateStory(event) {
   event.preventDefault();
-  console.log();
+
+  let apiKey = "5399eea49f9baa9t4a0de908084b4of2";
+  let prompt = "";
+  let context = "";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayStory);
 
   new Typewriter("#story", {
     strings: "Your story will be here",
