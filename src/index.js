0edf313fe7eec4e647d10ad6df5f88286fb9007d
MariaBerbis <maria.berbis.m@gmail.com>
2023-12-28 01:04:00 +0000

    Modify context AI

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ function generateStory(event) {
   let apiKey = "5399eea49f9baa9t4a0de908084b4of2";
   let prompt = `Generate a short story about ${instructionsTopicInput.value} and with ${instructionsCharacterInput.value}`;
   let context =
-    "You are great story teller and love to write short stories with a moral lesson. Your mission is to generate a 5 line story in basic HTML. Make sure to follow the user instructions and include the characters below. Please, sign each story with 'SheCodes AI' in <strong>. Generate the story in the same language the user's input is introduced.";
+    "You are great story teller and love to write short stories with a moral lesson. Your mission is to generate a 5 line story in basic HTML. Make sure to follow the user instructions and include the characters in story that you write below. Please, sign at the bottom of each story with 'SheCodes AI' in <strong>. Generate the story in the same language as the user's input.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("Generating a story...");
